929e54a30203b8c0ce230727c341f82aaeff8a93
Alexander <1alexvash@gmail.com>
2019-11-21 10:18:20 +0200

    Updates

---
+++ precache-manifest.ff298b07acf0dd63b0f2403abbc3a556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd60356fb95c4c9a714f1fbb5955f6d",
+    "url": "/Words/index.html"
+  },
+  {
+    "revision": "7b409397243a76c219c4",
+    "url": "/Words/static/css/main.c00120fc.chunk.css"
+  },
+  {
+    "revision": "98736fc32ed0fcb3b298",
+    "url": "/Words/static/js/2.216d6d8b.chunk.js"
+  },
+  {
+    "revision": "7b409397243a76c219c4",
+    "url": "/Words/static/js/main.8b4109dd.chunk.js"
+  },
+  {
+    "revision": "c423e402ab3c72a93359",
+    "url": "/Words/static/js/runtime-main.9041b32c.js"
+  },
+  {
+    "revision": "d61c552ed01862b9257b13b8a56c3f97",
+    "url": "/Words/static/media/return.d61c552e.png"
+  },
+  {
+    "revision": "7768c1d08a2ccb9be8d865eb81b94993",
+    "url": "/Words/static/media/sound_failure.7768c1d0.mp3"
+  },
+  {
+    "revision": "dcbb5b439d236a873f337fc600cf260c",
+    "url": "/Words/static/media/sound_success.dcbb5b43.mp3"
+  }
+]);
